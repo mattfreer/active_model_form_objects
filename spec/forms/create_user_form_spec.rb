83cd75f2948d74689151d2bd828fd73ba03fe164
matt.freer <matt.freer@gmail.com>
2013-12-23 16:07:48 +0000

    Feature: Attribute accessors creation
    Initialize method excepts a hash of attributes. Accessors are
    created for each attribute.

--- spec/forms/create_user_form_spec.rb
+++ spec/forms/create_user_form_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe CreateUserForm do
-  subject { described_class.new }
+  subject { described_class.new(attrs) }
+  let(:attrs) {}
 
   it_behaves_like "a form_object"
+
+  context "with attributes" do
+    let(:attrs) { {:foo => :bar} }
+    its(:foo) { should be :bar }
+  end
 end

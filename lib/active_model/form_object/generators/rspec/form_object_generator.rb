f5f528b904d8c21df87395af581a89b425285f5c
matt.freer <matt.freer@gmail.com>
2014-01-29 10:24:17 +0000

    Fix: Rspec generator hidden from Rails generator output

--- lib/active_model/form_object/generators/rspec/form_object_generator.rb
+++ lib/active_model/form_object/generators/rspec/form_object_generator.rb
@@ -3,7 +3,7 @@ require 'active_model/form_object/generators/test_generator'
 module Rspec
   module Generators
     class FormObjectGenerator < ActiveModel::Generators::TestGenerator
-      desc "Generate a FormObject spec in spec/form_objects/"
+      hide!
 
       source_root File.expand_path('../templates', __FILE__)
 

cec4133f0d7da3ecfba8aad1cffff9436ced927b
matt.freer <matt.freer@gmail.com>
2014-02-21 15:06:53 +0000

    Feature: Generated Form Objects include persist!
    Typically users will want to override the persist! method, hence a
    placeholder method is included in the generated code.

--- lib/active_model/form_object/generators/templates/form_object.rb
+++ lib/active_model/form_object/generators/templates/form_object.rb
@@ -3,5 +3,11 @@ require 'active_model'
 <% module_namespacing do -%>
 class <%= klass_name %><% if has_parent_class? %> < <%= parent_class_name %><% end %>
   include ActiveModel::FormObject
+
+  private
+
+  def persist!
+    #TODO
+  end
 end
 <% end -%>

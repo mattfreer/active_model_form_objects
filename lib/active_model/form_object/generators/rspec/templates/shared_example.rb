478304a377d6d73abe795aab906bb0b415dbc48b
matt.freer <matt.freer@gmail.com>
2014-02-24 10:13:03 +0000

    Chore: Shared example template upgrade to rspec 3
    The form_object shared example template has been upgraded to rspec 3

--- lib/active_model/form_object/generators/rspec/templates/shared_example.rb
+++ lib/active_model/form_object/generators/rspec/templates/shared_example.rb
@@ -1,42 +1,46 @@
 require 'spec_helper'
 
 shared_examples 'a form_object' do
-  its(:persisted?) { should be_false }
+  describe "#persisted?" do
+    it "should be false" do
+      expect(subject.persisted?).to eq false
+    end
+  end
 
   describe ".model_name" do
     it "should be instance of ActiveModel::Name" do
-      subject.class.model_name.should be_an_instance_of ActiveModel::Name
+      expect(subject.class.model_name).to be_an_instance_of ActiveModel::Name
     end
   end
 
   context "when valid" do
     before(:each) do
-      subject.stub(:valid?).and_return(true)
+      allow(subject).to receive(:valid?).and_return(true)
     end
 
     it "should receive #persist!" do
-      subject.should_receive(:persist!)
+      expect(subject).to receive(:persist!)
       subject.save
     end
 
     it "#save returns true" do
-      subject.stub(:persist!).and_return(true)
-      subject.save.should be_true
+      allow(subject).to receive(:persist!).and_return(true)
+      expect(subject.save).to eq(true)
     end
   end
 
   context "when invalid" do
     before(:each) do
-      subject.stub(:valid?).and_return(false)
+      allow(subject).to receive(:valid?).and_return(false)
     end
 
     it "shouldn't receive #persist!" do
-      subject.should_not_receive(:persist!)
+      expect(subject).not_to receive(:persist!)
       subject.save
     end
 
     it "#save returns false" do
-      subject.save.should be_false
+      expect(subject.save).to eq false
     end
   end
 end
